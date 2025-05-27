e00af8e96a88304ae29984cdfb8d546029f61c24
Kada Ozaki <kada02ak1@outlook.com>
2025-05-27 19:41:35 +0200

    update nixpkgs 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kada's NixOS system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     fenix = {
@@ -13,12 +13,12 @@
     crane.url = "github:ipetkov/crane";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix/release-24.11";
+      url = "github:danth/stylix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -129,6 +129,6 @@
     username = "kada";
     homeDirectory = "/home/kada";
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-    stateVersion = "24.11";
+    stateVersion = "25.05";
   };
 }
