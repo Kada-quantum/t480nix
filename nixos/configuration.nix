07e0fca81eb35b593f6443513c1ef67f1838ef21
Kada Ozaki <kada02ak1@outlook.com>
2025-08-16 13:40:01 +0200

    set home open-webui

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -175,6 +175,7 @@
       enable = true;
       package = pkgs.unstable.open-webui;
       environment = {
+        HOME = "/var/lib/open-webui";
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
